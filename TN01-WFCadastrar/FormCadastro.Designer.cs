ff20cc19ea7827b135f36acbdd3d30b0d8b6408f
JESSICA BATISTA DOS SANTOS <jessica.bsantos22@senacsp.edu.br>
2025-03-10 22:19:51 -0300

    Adicionar arquivos de projeto.

---
+++ TN01-WFCadastrar/FormCadastro.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace TN01_WFCadastrar
+{
+    partial class FormCadastro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormCadastro));
+            lblNomeCompleto = new Label();
+            txtNomeCompleto = new TextBox();
+            lblTelefone = new Label();
+            mkdTelefone = new MaskedTextBox();
+            lblDataNascimento = new Label();
+            dtpDataNascimento = new DateTimePicker();
+            lblEscolaridade = new Label();
+            cbxEscolaridade = new ComboBox();
+            gbxSexo = new GroupBox();
+            rdbNaoInformado = new RadioButton();
+            rdbFeminino = new RadioButton();
+            rdbMasculino = new RadioButton();
+            lblRenda = new Label();
+            nudRendaMensal = new NumericUpDown();
+            chkpPossuiFilhos = new CheckBox();
+            btnSalvar = new Button();
+            gbxSexo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)nudRendaMensal).BeginInit();
+            SuspendLayout();
+            // 
+            // lblNomeCompleto
+            // 
+            lblNomeCompleto.AutoSize = true;
+            lblNomeCompleto.Location = new Point(25, 47);
+            lblNomeCompleto.Name = "lblNomeCompleto";
+            lblNomeCompleto.Size = new Size(99, 15);
+            lblNomeCompleto.TabIndex = 0;
+            lblNomeCompleto.Text = "Nome Completo:";
+            // 
+            // txtNomeCompleto
+            // 
+            txtNomeCompleto.Location = new Point(25, 65);
+            txtNomeCompleto.Name = "txtNomeCompleto";
+            txtNomeCompleto.Size = new Size(377, 23);
+            txtNomeCompleto.TabIndex = 1;
+            // 
+            // lblTelefone
+            // 
+            lblTelefone.AutoSize = true;
+            lblTelefone.Location = new Point(22, 110);
+            lblTelefone.Name = "lblTelefone";
+            lblTelefone.Size = new Size(54, 15);
+            lblTelefone.TabIndex = 2;
+            lblTelefone.Text = "Telefone:";
+            // 
+            // mkdTelefone
+            // 
+            mkdTelefone.Location = new Point(29, 127);
+            mkdTelefone.Mask = "(99) 00000-0000";
+            mkdTelefone.Name = "mkdTelefone";
+            mkdTelefone.Size = new Size(169, 23);
+            mkdTelefone.TabIndex = 2;
+            // 
+            // lblDataNascimento
+            // 
+            lblDataNascimento.AutoSize = true;
+            lblDataNascimento.Location = new Point(218, 110);
+            lblDataNascimento.Name = "lblDataNascimento";
+            lblDataNascimento.Size = new Size(114, 15);
+            lblDataNascimento.TabIndex = 4;
+            lblDataNascimento.Text = "Data de Nascimento";
+            // 
+            // dtpDataNascimento
+            // 
+            dtpDataNascimento.Format = DateTimePickerFormat.Short;
+            dtpDataNascimento.Location = new Point(218, 127);
+            dtpDataNascimento.Name = "dtpDataNascimento";
+            dtpDataNascimento.Size = new Size(181, 23);
+            dtpDataNascimento.TabIndex = 3;
+            // 
+            // lblEscolaridade
+            // 
+            lblEscolaridade.AutoSize = true;
+            lblEscolaridade.Location = new Point(25, 178);
+            lblEscolaridade.Name = "lblEscolaridade";
+            lblEscolaridade.Size = new Size(73, 15);
+            lblEscolaridade.TabIndex = 6;
+            lblEscolaridade.Text = "Escolaridade";
+            // 
+            // cbxEscolaridade
+            // 
+            cbxEscolaridade.DropDownStyle = ComboBoxStyle.DropDownList;
+            cbxEscolaridade.FormattingEnabled = true;
+            cbxEscolaridade.Items.AddRange(new object[] { "Analfabeto", "Ensino Fundamental Incompleto", "Ensino Fundamental Completo", "Ensino Médio Incompleto", "Ensino Médio Completo", "Ensino Técnico Incompleto", "Ensino Técnico Completo", "Ensino Superior Incompleto", "Ensino Superior Completo", "Pós-Graduação Incompleto", "Pós-Graduação Completo" });
+            cbxEscolaridade.Location = new Point(29, 196);
+            cbxEscolaridade.Name = "cbxEscolaridade";
+            cbxEscolaridade.Size = new Size(370, 23);
+            cbxEscolaridade.TabIndex = 4;
+            // 
+            // gbxSexo
+            // 
+            gbxSexo.Controls.Add(rdbNaoInformado);
+            gbxSexo.Controls.Add(rdbFeminino);
+            gbxSexo.Controls.Add(rdbMasculino);
+            gbxSexo.Location = new Point(32, 258);
+            gbxSexo.Name = "gbxSexo";
+            gbxSexo.Size = new Size(152, 108);
+            gbxSexo.TabIndex = 5;
+            gbxSexo.TabStop = false;
+            gbxSexo.Text = "Sexo";
+            // 
+            // rdbNaoInformado
+            // 
+            rdbNaoInformado.AutoSize = true;
+            rdbNaoInformado.Location = new Point(6, 75);
+            rdbNaoInformado.Name = "rdbNaoInformado";
+            rdbNaoInformado.Size = new Size(134, 19);
+            rdbNaoInformado.TabIndex = 2;
+            rdbNaoInformado.TabStop = true;
+            rdbNaoInformado.Text = "Prefiro Não Informar";
+            rdbNaoInformado.UseVisualStyleBackColor = true;
+            // 
+            // rdbFeminino
+            // 
+            rdbFeminino.AutoSize = true;
+            rdbFeminino.Location = new Point(6, 51);
+            rdbFeminino.Name = "rdbFeminino";
+            rdbFeminino.Size = new Size(75, 19);
+            rdbFeminino.TabIndex = 1;
+            rdbFeminino.TabStop = true;
+            rdbFeminino.Text = "Feminino";
+            rdbFeminino.UseVisualStyleBackColor = true;
+            // 
+            // rdbMasculino
+            // 
+            rdbMasculino.AutoSize = true;
+            rdbMasculino.Location = new Point(6, 26);
+            rdbMasculino.Name = "rdbMasculino";
+            rdbMasculino.Size = new Size(80, 19);
+            rdbMasculino.TabIndex = 0;
+            rdbMasculino.TabStop = true;
+            rdbMasculino.Text = "Masculino";
+            rdbMasculino.UseVisualStyleBackColor = true;
+            // 
+            // lblRenda
+            // 
+            lblRenda.AutoSize = true;
+            lblRenda.Location = new Point(229, 258);
+            lblRenda.Name = "lblRenda";
+            lblRenda.Size = new Size(81, 15);
+            lblRenda.TabIndex = 9;
+            lblRenda.Text = "Renda Mensal";
+            // 
+            // nudRendaMensal
+            // 
+            nudRendaMensal.DecimalPlaces = 2;
+            nudRendaMensal.Location = new Point(229, 276);
+            nudRendaMensal.Maximum = new decimal(new int[] { 9999, 0, 0, 0 });
+            nudRendaMensal.Name = "nudRendaMensal";
+            nudRendaMensal.Size = new Size(120, 23);
+            nudRendaMensal.TabIndex = 6;
+            // 
+            // chkpPossuiFilhos
+            // 
+            chkpPossuiFilhos.AutoSize = true;
+            chkpPossuiFilhos.Checked = true;
+            chkpPossuiFilhos.CheckState = CheckState.Indeterminate;
+            chkpPossuiFilhos.Location = new Point(229, 333);
+            chkpPossuiFilhos.Name = "chkpPossuiFilhos";
+            chkpPossuiFilhos.Size = new Size(91, 19);
+            chkpPossuiFilhos.TabIndex = 7;
+            chkpPossuiFilhos.Text = "Possui Fihos";
+            chkpPossuiFilhos.UseVisualStyleBackColor = true;
+            // 
+            // btnSalvar
+            // 
+            btnSalvar.Image = (Image)resources.GetObject("btnSalvar.Image");
+            btnSalvar.ImageAlign = ContentAlignment.MiddleLeft;
+            btnSalvar.Location = new Point(169, 400);
+            btnSalvar.Name = "btnSalvar";
+            btnSalvar.Size = new Size(75, 34);
+            btnSalvar.TabIndex = 8;
+            btnSalvar.Text = "Salvar";
+            btnSalvar.TextAlign = ContentAlignment.MiddleRight;
+            btnSalvar.UseVisualStyleBackColor = true;
+            btnSalvar.Click += btnSalvar_Click;
+            // 
+            // FormCadastro
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(428, 467);
+            Controls.Add(btnSalvar);
+            Controls.Add(chkpPossuiFilhos);
+            Controls.Add(nudRendaMensal);
+            Controls.Add(lblRenda);
+            Controls.Add(gbxSexo);
+            Controls.Add(cbxEscolaridade);
+            Controls.Add(lblEscolaridade);
+            Controls.Add(dtpDataNascimento);
+            Controls.Add(lblDataNascimento);
+            Controls.Add(mkdTelefone);
+            Controls.Add(lblTelefone);
+            Controls.Add(txtNomeCompleto);
+            Controls.Add(lblNomeCompleto);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "FormCadastro";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "FormCadastro";
+            gbxSexo.ResumeLayout(false);
+            gbxSexo.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)nudRendaMensal).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblNomeCompleto;
+        private TextBox txtNomeCompleto;
+        private Label lblTelefone;
+        private MaskedTextBox mkdTelefone;
+        private Label lblDataNascimento;
+        private DateTimePicker dtpDataNascimento;
+        private Label lblEscolaridade;
+        private ComboBox cbxEscolaridade;
+        private GroupBox gbxSexo;
+        private RadioButton rdbNaoInformado;
+        private RadioButton rdbFeminino;
+        private RadioButton rdbMasculino;
+        private Label lblRenda;
+        private NumericUpDown nudRendaMensal;
+        private CheckBox chkpPossuiFilhos;
+        private Button btnSalvar;
+    }
+}
